c276771a2c807dc30672350b55935ec72b7558f3
p4stoboy <43506721+p4stoboy@users.noreply.github.com>
2024-03-20 16:05:08 +1100

    fixed force application

--- Settings.h
+++ Settings.h
@@ -1,7 +1,7 @@
 // settings
 #define MIN_BLOCK_FRAME_MOD 3
 #define MAX_BLOCK_FRAME_MOD 60
-#define MIN_BLOCK_VEL -1.0
-#define MAX_BLOCK_VEL 1.0
-#define MIN_BLOCK_ACC -0.01
-#define MAX_BLOCK_ACC 0.01
+#define MIN_BLOCK_VEL -2.0
+#define MAX_BLOCK_VEL 2.0
+#define MIN_BLOCK_ACC -0.02
+#define MAX_BLOCK_ACC 0.02
