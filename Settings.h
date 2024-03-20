e469edac9eda4ae9faf689f95c7d85c422fe0f11
p4stoboy <43506721+p4stoboy@users.noreply.github.com>
2024-03-21 09:27:31 +1100

    parameter changes

--- Settings.h
+++ Settings.h
@@ -1,7 +1,7 @@
 // settings
 #define MIN_BLOCK_FRAME_MOD 3
 #define MAX_BLOCK_FRAME_MOD 60
-#define MIN_BLOCK_VEL -3.0
-#define MAX_BLOCK_VEL 3.0
+#define MIN_BLOCK_VEL -2.0
+#define MAX_BLOCK_VEL 2.0
 #define MIN_BLOCK_ACC -0.1
 #define MAX_BLOCK_ACC 0.1
